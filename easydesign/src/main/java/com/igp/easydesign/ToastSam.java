b9efdc83a7d734688158378923814908ce990e26
邱爱詹 <chiuzhengqun@gmail.com>
2018-07-15 11:52:01 +0800

    Initial commit

---
+++ easydesign/src/main/java/com/igp/easydesign/ToastSam.java
@@ -0,0 +1,14 @@
+package com.igp.easydesign;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by qiu on 2018/7/15.
+ */
+
+public class ToastSam {
+    public void toast(Context context){
+        Toast.makeText(context, "Testing", Toast.LENGTH_SHORT).show();
+    }
+}

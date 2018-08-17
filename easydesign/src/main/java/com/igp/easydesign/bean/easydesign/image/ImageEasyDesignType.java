d4e95e638f57e8872a0681acde5fc982136702dd
Sam <694665111@qq.com>
2018-08-17 15:28:46 +0800

    图片设计类型增加了远程背景和远程相框的类型

--- easydesign/src/main/java/com/igp/easydesign/bean/easydesign/image/ImageEasyDesignType.java
+++ easydesign/src/main/java/com/igp/easydesign/bean/easydesign/image/ImageEasyDesignType.java
@@ -7,7 +7,10 @@ package com.igp.easydesign.bean.easydesign.image;
 public enum ImageEasyDesignType{
     REMOTE_ALBUM     (0),
     STICKER          (1),
-    LOCAL_ALBUM      (2);
+    LOCAL_ALBUM      (2),
+    REMOTE_BACKGROUND(3),
+    REMOTE_FRAME     (4);
+
     ImageEasyDesignType(int ni) {
         nativeInt = ni;
     }
